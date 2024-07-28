ea3f2ee6a883e83ceb1600e4bd12e2538ab6a236
mxsgx <mxsgx.id@gmail.com>
2024-07-28 22:52:37 +0700

    feat: run migrations on first startup

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This script checks if the container is started for the first time.
+
+CONTAINER_FIRST_STARTUP="CONTAINER_FIRST_STARTUP"
+if [ ! -e /$CONTAINER_FIRST_STARTUP ]; then
+    touch /$CONTAINER_FIRST_STARTUP
+    # place your script that you only want to run on first startup.
+    poetry run alembic upgrade head && poetry run uvicorn --host 0.0.0.0 --port 8000 main:app --reload
+else
+    # script that should run the rest of the times (instances where you 
+    # stop/restart containers).
+    poetry run uvicorn --host 0.0.0.0 --port 8000 main:app --reload
+fi
