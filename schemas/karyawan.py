4a90d0dd28c8fdfc8b88b137f33adc633e4c5f4b
addthoriq <alvarro.thoriq@gmail.com>
2024-08-03 21:36:50 +0700

    setup schemas for User, Karyawan, Guru

--- schemas/karyawan.py
+++ schemas/karyawan.py
@@ -3,26 +3,26 @@ from pydantic import BaseModel, StringConstraints, field_validator
 from models import factory_session
 from models.User import User
 
-class PaginateGuruResponse(BaseModel):
+class PaginateKaryawanResponse(BaseModel):
     counts: int
     page_count: int
     page_size: int
     page: int
     
-    class DetailGuruResponse(BaseModel):
+    class DetailKaryawanResponse(BaseModel):
         id: int
         email: str
-        nama_guru: str
+        nama_karyawan: str
     
-    result: List[DetailGuruResponse]
+    result: List[DetailKaryawanResponse]
     
-class DetailGuruResponse(BaseModel):
+class DetailKaryawanResponse(BaseModel):
     id: int
     email: str
-    nama_guru: str
+    nama_karyawan: str
 
-class CreateGuruRequest(BaseModel):
-    nama_guru: Annotated[str, StringConstraints(min_length=1, max_length=50)]
+class CreateKaryawanRequest(BaseModel):
+    nama_karyawan: Annotated[str, StringConstraints(min_length=1, max_length=50)]
     email: Annotated[str, StringConstraints(min_length=1, max_length=30)]
     password: Annotated[str, StringConstraints(min_length=1, max_length=50)]
 
@@ -34,19 +34,19 @@ class CreateGuruRequest(BaseModel):
                 raise ValueError("This email already exist!")
         return email
     
-class CreateGuruResponse(BaseModel):
+class CreateKaryawanResponse(BaseModel):
     id: str
-    nama_guru: str
+    nama_karyawan: str
     email: str
     
-class UpdateGuruRequest(BaseModel):
-    nama_guru: str
+class UpdateKaryawanRequest(BaseModel):
+    nama_karyawan: str
     email: str
 
-class UpdateGuruValidation(BaseModel):
+class UpdateKaryawanValidation(BaseModel):
     id: str
     email: Annotated[str, StringConstraints(min_length=1, max_length=30)]
-    nama_guru: Annotated[str, StringConstraints(min_length=1, max_length=50)]
+    nama_karyawan: Annotated[str, StringConstraints(min_length=1, max_length=50)]
     
     @field_validator("email")
     def validate_email_unique(cls, email, value):
@@ -62,9 +62,9 @@ class UpdateGuruValidation(BaseModel):
                     raise ValueError("This email already exists!")
         return email
 
-class UpdateGuruResponse(BaseModel):
+class UpdateKaryawanResponse(BaseModel):
     id: int
-    nama_guru: str
+    nama_karyawan: str
     email: str
     
 class ChangePasswordRequest(BaseModel):
---
+++ schemas/user.py
@@ -0,0 +1,104 @@
+from typing import List, Annotated, Optional
+from pydantic import BaseModel, StringConstraints, field_validator
+from models import factory_session
+from models.User import User
+
+class PaginateUserResponse(BaseModel):
+    counts: int
+    page_count: int
+    page_size: int
+    page: int
+    
+    class DetailUserResponse(BaseModel):
+        id: int
+        email: str
+        nama_user: str
+        
+        class DetailJabatan(BaseModel):
+            id: int
+            nama_jabatan: str
+        jabatan: Optional[DetailJabatan]
+    
+    result: List[DetailUserResponse]
+    
+class DetailUserResponse(BaseModel):
+    id: int
+    email: str
+    nama_user: str
+
+    class DetailJabatan(BaseModel):
+        id: int
+        nama_jabatan: str
+    jabatan: Optional[DetailJabatan]
+
+class CreateUserRequest(BaseModel):
+    nama_user: Annotated[str, StringConstraints(min_length=1, max_length=50)]
+    email: Annotated[str, StringConstraints(min_length=1, max_length=30)]
+    password: Annotated[str, StringConstraints(min_length=1, max_length=50)]
+    jabatan: list[int]
+
+    @field_validator("email")
+    def validate_email_unique(cls, email):
+        with factory_session() as session:
+            existing_name = session.query(User).filter_by(email=email).first()
+            if existing_name:
+                raise ValueError("This email already exist!")
+        return email
+    
+class CreateUserResponse(BaseModel):
+    id: str
+    nama_user: str
+    email: str
+
+    class DetailJabatan(BaseModel):
+        id: int
+        nama_jabatan: str
+    jabatan: Optional[DetailJabatan]
+    
+    
+class UpdateUserRequest(BaseModel):
+    nama_user: str
+    email: str
+    jabatan: list[int]
+
+class UpdateUserValidation(BaseModel):
+    id: str
+    email: Annotated[str, StringConstraints(min_length=1, max_length=30)]
+    nama_user: Annotated[str, StringConstraints(min_length=1, max_length=50)]
+    
+    @field_validator("email")
+    def validate_email_unique(cls, email, value):
+        current_id = value.data.get("id")
+        if email is not None:
+            with factory_session() as session:
+                existing_user = (
+                    session.query(User)
+                    .filter(User.email == email, User.id is not current_id)
+                    .first()
+                )
+                if existing_user:
+                    raise ValueError("This email already exists!")
+        return email
+
+class UpdateUserResponse(BaseModel):
+    id: int
+    nama_user: str
+    email: str
+    class DetailJabatan(BaseModel):
+        id: int
+        nama_jabatan: str
+    jabatan: Optional[DetailJabatan]
+    
+class ChangePasswordRequest(BaseModel):
+    password: Annotated[str, StringConstraints(min_length=1, max_length=50)]
+
+class PaginateJabatanResponse(BaseModel):
+    counts: int
+    page_count: int
+    page_size: int
+    page: int
+    
+    class DetailJabatan(BaseModel):
+        id: int
+        nama_jabatan: str
+    jabatan: Optional[DetailJabatan]
